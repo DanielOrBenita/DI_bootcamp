bfa380167f635caa74722ec3e60a35f7882e0b02
danielorbenita <danielorbenita@gmail.com>
2022-04-23 22:54:15 +0300

    hackaton ready

--- PlayGround/BurgerMenu/js.js
+++
@@ -1,8 +0,0 @@
-let btn= document.getElementById(`btn`)
-let nav= document.getElementById(`nav`)
-let ul= document.getElementById(`ul`)
-
-btn.addEventListener(`click`, ()=>{
-    nav.classList.toggle(`active`)
-    btn.classList.toggle(`active`)
-})
