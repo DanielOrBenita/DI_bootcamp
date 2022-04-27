1eb2630466a0153545cb61b3220493381a86c6bc
danielorbenita <danielorbenita@gmail.com>
2022-04-27 23:06:17 +0300

    dailyC W6D4

---
+++ Week 6/Day4/index.js
@@ -0,0 +1,31 @@
+let client = "betty"; // was jhon at first.
+
+let groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "35$", // was 20$ at first
+    other : {
+        payed : false, // was true at first
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+//1 ex1 
+
+let displayGroceries= ()=> groceries.fruits.forEach((e) => console.log(e))
+
+displayGroceries()
+
+//ex2
+
+let cloneGroceries= ()=>{
+    let user=client
+    console.log(user) // it will change to `betty` because it pass by reference and not by value.
+    
+    let shopping = groceries
+    console.log(shopping) // the total price value will change because it pass by reference.
+
+    // we will no see any effect because we copied only the first obj but thers another obj inside the obj to reference to it we need to do something with JSON
+    // which we did not study yet.
+}
+cloneGroceries()
