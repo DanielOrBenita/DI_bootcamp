702d3bd4ff135e7a0104caf592ea6d8e265db423
DanielOrBenita <Danielorbenita@gmail.com>
2022-06-30 17:25:02 +0300

    week 14 dailyc

---
+++ Week14/Day2/dailyC/dailyc/src/App.js
@@ -0,0 +1,12 @@
+import CarouselComponent from "./CarouselComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselComponent />
+    </div>
+  );
+}
+
+export default App;
